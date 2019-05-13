a36bed45abf7ca6067adcceec6ae444518b95ea7
veba <fastbakken@gmail.com>
2019-05-13 12:16:26 +0200

    Update test_main.c

--- test_main.c
+++ test_main.c
@@ -1,1 +1,31 @@
+#include <stdint.h>
+#include <stdbool.h>
 
+#include "workq.h"
+
+WORKQ_DECLARE(mainq);
+WORKQ_ITEM_DECLARE(item1);
+
+
+void fun1(struct workq_item *itm)
+{
+}
+
+
+int main(void)
+{
+  
+  #define WORKQ_DECLARE(__name__)       \
+                static struct workq wq_##__name__
+
+  workq_init(&wq_main);
+  workq_item_init(&wqi_item1, fun1);
+  workq_post(&wq_main, &wqi_item1);
+  while (1) {
+    if (workq_iterate(&wq_main) == 0) {
+      //Sleep
+    } 
+  }
+  
+  return 0;
+}
