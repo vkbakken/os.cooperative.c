06edfa679e25e04c1d5b064616ad027623c13ea8
liemleba <liemle@mpire.vn>
2019-05-16 12:08:34 +0700

    Update workq post and cancel interface

--- workq.c
+++ workq.c
@@ -1,6 +1,10 @@
 #include "workq.h"
 #include "os_assert.h"
 
+void workq_init(struct workq *q){
+
+}
+
 void workq_item_init(struct workq_item *w,
 		void (*fun)(struct workq_item * work))
 {
@@ -24,12 +28,20 @@ void workq_post(struct workq *q, struct workq_item *w)
 	__ASSERT(NULL != q);
 	__ASSERT(NULL != w);
 
+	//TODO: where should insert the new item to?
+
+	// insert to the end of slist
 	if (workq_is_empty(q)) {
 		q->end = w;
+		q->start = q->end;
 	} else {
 		q->end->next = w;
 		q->end = w;
 	}
+
+	//insert to the start of slist
+//	w->next = q->start;
+//	q->start = w;
 }
 
 void workq_post_delayed(struct workq *q, struct workq_item *w, uint32_t dly)
@@ -45,16 +57,16 @@ void workq_cancel(struct workq *q, struct workq_item *w)
 	__ASSERT(NULL != q);
 	__ASSERT(NULL != w);
 
-	struct workq_item **iterator = &(q->start);
-
-	if (*(iterator) == w) {
-		*(iterator) = NULL;
+	if (q->start == w) {
+		q->start = w;
 	} else {
-		while ((*(iterator))->next != NULL) {
-			if ((*(iterator))->next == w) {
-				(*(iterator))->next = (*(iterator))->next->next;
+		struct workq_item **iterator = &(q->start);
+		while ((*iterator) != NULL) {
+			if ((*iterator)->next == w) {
+				(*iterator)->next = w->next;
+				break;
 			}
-			iterator = &((*iterator)->next);
+			iterator = &(*iterator)->next;
 		}
 	}
 }
@@ -64,8 +76,6 @@ uint32_t workq_iterate(struct workq *q)
 	__ASSERT(NULL != q);
 
 	struct workq_item **iterator = &(q->start);
-
-
 
 	return 0;
 }
